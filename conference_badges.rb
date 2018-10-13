569a5fb3ae0c354fe5ec345aacecd5d48c721a6a
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-13 19:09:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,9 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(attendees)
-  #batch_badge_creator(attendees) +
+  #batch_badge_creator(attendees) 
   attendees.map.with_index{|attendee,i| "Hello, #{attendee}! You'll be assigned to room #{i+1}!"}
-  #puts attendees.map_with_index{|attendee,i| puts "Hello, #{attendee}! You'll be assigned to room #{i+1}!"}
+
 end
 
-#puts assign_rooms(["Steve"])
+def printer
